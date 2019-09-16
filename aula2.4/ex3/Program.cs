379adc5b2692812a641a865fd4923cbf47ec2ae4
FIC <FIC@info.local>
2019-09-16 15:19:47 -0300

    Exe3: Sistemas de Decisão (maior valor)

---
+++ aula2.4/ex3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int var1, var2;
+
+            Console.Write("Escreva o primeiro valor: ");
+            var1 = int.Parse(Console.ReadLine());
+            
+            Console.Write("Escreva o primeiro valor: ");
+            var2 = int.Parse(Console.ReadLine());
+
+            if( var1 > var2){
+                Console.Write($"O maior numero é {var1}");
+            }else {
+                Console.Write($"O maior numero é {var2}");
+            }
+        
+        }
+    }
+}
