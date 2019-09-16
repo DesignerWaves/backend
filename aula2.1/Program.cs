a73976d0b43b4d858598e5647e1793bc84d9488b
FIC <FIC@info.local>
2019-09-16 12:29:42 -0300

    HomeWork Update

--- aula2.1/Program.cs
+++ aula2.1/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 
-namespace aula2._1
+namespace aula_2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Declaramos as notas que 
+            float  nota1, nota2, nota3, media;
+            string resultado;
+
+            // Capturamos a 1º nota
+            Console.Write("Digite a sua primeira nota: ");
+            nota1 = float.Parse( Console.ReadLine());
+
+            // Capturamos a 2º nota
+            Console.Write("Digite a sua segunda nota: ");
+            nota2 = float.Parse( Console.ReadLine());
+
+            // Capturamos a 3º nota
+            Console.Write("Digite a sua terceira nota: ");
+            nota3 = float.Parse( Console.ReadLine());
+
+            //Tratamento dos dados  
+            media = (nota1+nota2+nota3) / 3;
+
+            //Verificamos se a media do aluno é maior ou igual
+            if( media >= 6 ){
+                resultado = "aprovado";
+            } else if( media < 6 && media >= 4 ){
+                resultado = "Recuperação";
+            }else{
+                resultado = "Reprovado";
+            }
+
+            Console.WriteLine($"Média : {media} - Aluno {resultado} ");
         }
     }
 }
