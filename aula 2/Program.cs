cafaff8a688627fb48ab3d68a325d57dac3c1e43
FIC <FIC@info.local>
2019-09-16 12:05:23 -0300

    first commit

---
+++ aula 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace aula_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaramos as notas que 
+            double nota1, nota2, nota3, media;
+
+            // Capturamos a 1º nota
+            Console.Write("Digite a sua primeira nota: ");
+            nota1 = double.Parse( Console.ReadLine());
+
+            // Capturamos a 2º nota
+            Console.Write("Digite a sua primeira nota: ");
+            nota2 = double.Parse( Console.ReadLine());
+
+            // Capturamos a 3º nota
+            Console.Write("Digite a sua primeira nota: ");
+            nota3 = double.Parse( Console.ReadLine());
+
+            //Tratamento dos dados  
+            media = (nota1+nota2+nota3) / 3;
+
+            //Verificamos se a media do aluno é maior ou igual
+            if( media >= 6 ){
+                Console.WriteLine($"A media do aluno foi {media}, logo o aluno está aprovado");
+
+            } else {
+                Console.WriteLine($"A media do aluno foi {media}, logo o aluno está reprovado");
+            }
+
+        }
+    }
+}
---
+++ aula1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace aula1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            // somente declaramos sem atribuir valor
+            int num1;
+            
+            // Declaramos atribuindo um valor
+            int num2=5;
+
+            // Declaramos diversas variaveis do mesmo tipo e eu posso ou nao atribuir valor a ela
+            int num3, num4, num5= 8;
+
+            // Variavel do tipo decimal (real)
+            float media;
+    
+            // Variavel do tipo logico
+            bool resultado = false;
+
+            Console.WriteLine(num2 + num5)
+        }
+    }
+}
