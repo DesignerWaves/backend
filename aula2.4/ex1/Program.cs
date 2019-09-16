6afe2e1733119cbc398b9337e6f4e0f68a66f664
FIC <FIC@info.local>
2019-09-16 14:43:46 -0300

    Ex1 Sistema de decisão

---
+++ aula2.4/ex1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sal, desptotal, var1;
+            
+            Console.Write("Digite seu salário: ");
+            sal = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite suas despesas mensais: ");
+            desptotal = double.Parse(Console.ReadLine());
+
+            var1 = sal - desptotal;
+
+            if( var1 > 0 ) {
+                Console.WriteLine("Seu saldo é positivo");
+            }else{
+                Console.WriteLine("Seu saldo é negativo");
+            }
+            
+        }
+    }
+}
