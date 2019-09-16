e1214fe3db1c092b981e6d2275d16b1e6dee5ba0
FIC <FIC@info.local>
2019-09-16 15:02:59 -0300

    Exe2: Sitema de decisão (descontos de loja)

---
+++ aula2.4/ex2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double var1, total1;
+
+            Console.Write("Quantos você gastou: ");
+            var1 = Double.Parse(Console.ReadLine());
+
+            if( var1 < 100) {
+                total1 = var1 - 20;
+                Console.Write($"O valor com desconto será: {total1} ");
+            }else{
+                Console.Write("Não terá desconto pois o valor foi muito alto");
+            }
+        }
+    }
+}
