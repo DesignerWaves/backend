bcaec201ab022fb54d45d6953ba25144c8e927b3
FIC <FIC@info.local>
2019-09-17 11:23:36 -0300

    Ex1: Switch Case  (Lanches e pedidos)

---
+++ aula2.5/ex1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string pedido, numero;
+            
+            Console.Write("Digite o numero do seu pedido: ");
+            numero = Console.ReadLine();
+
+
+            switch(numero){
+
+            case "1":
+            pedido = "Switch - Hamburguer";
+            break;
+
+            case "2":
+            pedido = "Switch - Cheese Salada";
+            break;
+
+             case "3":
+            pedido = "Switch - Cheese Burger";
+            break;
+
+             case "4":
+            pedido = "Switch - Cheese Bacon";
+            break;
+
+            default:
+            pedido = "Switch - Entrada inválida! :(";
+            break;
+
+
+            }
+
+            Console.Write(pedido);
+
+        }
+    }
+}
