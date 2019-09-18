2723f7139138898ee4ed0f4fbd2118803a5be875
FIC <FIC@info.local>
2019-09-18 14:22:05 -0300

    Ex1: Switch Case  (Lanches e pedidos) - Update

---
+++ Explicação/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace aula2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string placa;
+            string final;
+            string resultado;
+
+        Console.Write("Digite a placa do seu carro: ");
+        placa = Console.ReadLine();
+
+
+        //Contamos a quantidades de caracteres de um elemento
+        int caracteres = placa.Length;
+
+        //Pegamos o ultimo digito com o metodo substring();
+        final = placa.Substring(caracteres - 1);
+
+        if(final == "1" || final == "2") {
+            resultado = "Seu rodizio é na segunda-feira";
+        }else if(final == "3" || final == "4"){
+            resultado = "Seu rodizio é na terça-feira";
+        }else if(final == "5" || final == "6"){
+            resultado = "Seu rodizio é na quarta-feira";
+        }else if(final == "7" || final == "8"){
+            resultado = "Seu rodizio é na quinta-feira";
+        }else if(final == "9" || final == "0"){
+            resultado = "Seu rodizio é na sexta-feira";
+        }else{
+            resultado = "Placa inválida!";
+        }
+
+        Console.Write(resultado);
+
+        switch(final){
+
+            case "1":
+            resultado = "Switch - Segunda-Feira";
+            break;
+
+             case "2":
+            resultado = "Switch - Segunda-Feira";
+            break;
+
+             case "3":
+            resultado = "Switch - Terça-Feira";
+            break;
+
+             case "4":
+            resultado = "Switch - Terça-Feira";
+            break;
+
+             case "5":
+            resultado = "Switch - Quarta-Feira";
+            break;
+
+             case "6":
+            resultado = "Switch - Quarta-Feira";
+            break;
+
+             case "7":
+            resultado = "Switch - Quinta-Feira";
+            break;
+
+             case "8":
+            resultado = "Switch - Quinta-Feira";
+            break;
+
+             case "9":
+            resultado = "Switch - Sexta-Feira";
+            break;
+
+             case "0":
+            resultado = "Switch - Sexta-Feira";
+            break;
+
+            default:
+            resultado = "Switch - Entrada inválida! :(";
+            break;
+        }
+        
+        }
+    }
+}
---
+++ aula2.5/ex2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double peso, planeta, pesototal;
+
+
+            Console.Write("Digite seu peso: ");
+            peso = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o número do planeta: ");
+            planeta = double.Parse(Console.ReadLine());
+
+
+
+switch(numero){
+
+            case "1":
+            pesototal = (peso/10) * 0.37;
+            break;
+
+            case "2":
+            pedido = "Switch - Cheese Salada";
+            break;
+
+             case "3":
+            pedido = "Switch - Cheese Burger";
+            break;
+
+             case "4":
+            pedido = "Switch - Cheese Bacon";
+            break;
+
+            default:
+            pedido = "Switch - Entrada inválida! :(";
+            break;
+
+        }
+    }
+}
