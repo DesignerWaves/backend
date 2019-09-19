eb6e940dae4ca88c90450338fa2cdea1363f8552
FIC <FIC@info.local>
2019-09-19 12:24:05 -0300

    While, Do While

--- aula3/Program.cs
+++ aula3/Program.cs
@@ -1,28 +1,69 @@
 ﻿using System;
 
-namespace aula3
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace aula3 {
+    class Program {
+        static void Main (string[] args) {
             //laço contato - for - incremento (+)
-            for(int cont= 0 ; cont <= 100 ; cont++){
-                
-                // verificamos se o numero é diferente de :
-                // cont % tem como fnalidade pegar verificar o resto da divisão e juntamente com o != (diferente) faz com que o pegue todos os numeros que nao tenham um valor exato como resultado e os exiba na tela
+            // for(int cont= 0 ; cont <= 100 ; cont++){
 
-                if(cont % 2 != 0){
-                    Console.WriteLine(cont);
+            //     // verificamos se o numero é diferente de :
+            //     // cont % tem como fnalidade pegar verificar o resto da divisão e juntamente com o != (diferente) faz com que o pegue todos os numeros que nao tenham um valor exato como resultado e os exiba na tela
 
+            //     if(cont % 2 != 0){
+            //         Console.WriteLine(cont);
+
+            //     }
+            // }
+            // //Lçao contado - for - decremetno (-)
+            // for (int cont = 100 ; cont >= 0 ; cont--){
+            //     if(cont % 2 != 0){
+            //         Console.WriteLine("For 100 - 0 " + cont);
+            //     }
+            // }
+
+            // Laço condição - while
+            /*
+            double nota = 0;
+            string sair = "";
+            double acumu = 0;
+            int cont = 0;
+            while ( sair != "sim") {
+                Console.Write ("Digite a nota de um aluno: ");
+                nota = double.Parse (Console.ReadLine ());
+                if (nota != 1000) {
+                    acumu += nota;
+                    cont++;
+
+                    Console.Write ("Deseja sair da aplicação? Sim/Nao");
+                    sair = Console.ReadLine ().ToLower ();
                 }
-            }
-            //Lçao contado - for - decremetno (-)
-            for (int cont = 100 ; cont >= 0 ; cont--){
-                if(cont % 2 != 0){
-                    Console.WriteLine("For 100 - 0 " + cont);
+                Console.Write ("medias das notas " + (acumu / cont));
+
+            } */
+
+            /*
+            Problema:
+
+            faça um algoritimo que leia 20 numeros inteiros e acumule os numeros multiplos de 3
+            */
+
+            // Laçocondicional do / while
+            int cont = 1, acumu = 0, var1 = 0;
+
+            do {
+                Console.WriteLine ("Escreva um numero: ");
+                var1 = int.Parse (Console.ReadLine ());
+                if (var1 % 3  == 0) {
+                    acumu += var1;
                 }
+
+                cont++;
+            } while (cont <= 3); {
+                Console.WriteLine (acumu);
+
             }
+
         }
+
     }
 }
---
+++ aula3/fixacao/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace fixacao {
+    class Program {
+        static void Main (string[] args) {
+            int num1 = 0, acumu = 0, cont = 1;
+
+            do {
+                Console.Write ("Digite um numero inteiro:\n ");
+                num1 = int.Parse (Console.ReadLine ());
+                if (num1 % 5 == 0) {
+                    acumu += num1;
+                }
+                cont++;
+            } while (cont <= 3);
+            Console.Write (acumu);
+        }
+    }
+}
