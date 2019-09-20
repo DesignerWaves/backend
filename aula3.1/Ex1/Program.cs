b5ac27258b49acc338195d40c833b28f9436e33d
FIC <FIC@info.local>
2019-09-20 15:52:50 -0300

    Uptade lições vetores

--- aula3.1/Ex1/Program.cs
+++ aula3.1/Ex1/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 
-namespace Ex1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Ex1 {
+    class Program {
+        static void Main (string[] args) {
+            int[] var1 = new int[10];
+            int[] var2 = new int[10];
+
+            for (int i = 0; i <= 9; i++) {
+                Console.WriteLine ("Digite um valor: ");
+                var1[i] = int.Parse (Console.ReadLine ());
+            }
+            for (int i = 0; i <= 9; i++) {
+                var2[i] = var1[i];
+            }
+
+            for (int i = 0 ; i <= 9 ; i++){
+                Console.WriteLine(var2[i]);
+            }
+            Console.WriteLine(var2[9] * 5);
         }
     }
 }
