b52820892b4ea7bb9ddda7dc08af570e5aadf584
FIC <FIC@info.local>
2019-09-24 16:04:04 -0300

    Validação de CNPJ (HomeWork) - C#

---
+++ Aula6/ex_CNPJ/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ex_CNPJ {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Digite o CNPJ: ");
+
+            Console.WriteLine ()
+        }
+
+        static bool validarCNPJ (string CNPJusuario) {
+            bool resultado = false;
+            int[] v1 = { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4 };
+            string CNPJcalculo = 0;
+            int resto = 0;
+            int calculo = 0;
+            string digite_v1 = "";
+            string digite_v2 = "";
+
+            CPNJcalculo = CNPJUsuario.Subtring (0, 12);
+
+            CNPJusuario = CNPJusuario.Trim('.');
+            CNPJUsuario =  CNPJusuario.Trim('-');
+
+        }
+    }
+}
